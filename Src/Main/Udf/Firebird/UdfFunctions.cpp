f0a441d7ac145fc1058af473a22d4b3846b88fd7
Magno Machado Paulo <magnomp@gmail.com>
2010-10-24 15:34:05 -0200

    Estrutura inicial de pastas e primeiros fontes da UDF para Firebird

---
+++ Src/Main/Udf/Firebird/UdfFunctions.cpp
@@ -0,0 +1,33 @@
+#include "UdfFunctions.h"
+
+int begin_procedure(char *name) {
+
+}
+
+int begin_trigger(char *name) {
+
+}
+
+int begin_insert(char *name) {
+
+}
+
+int begin_update(char *name) {
+
+}
+
+int begin_delete(char *name) {
+
+}
+
+int end_operation(void) {
+
+}
+
+int begin_profile(void) {
+
+}
+
+int end_profile(char *logfile) {
+
+}
---
+++ Src/Main/Udf/Firebird/UdfFunctions.h
@@ -0,0 +1,10 @@
+extern "C" {
+    int begin_procedure(char *name);
+    int begin_trigger(char *name);
+    int begin_insert(char *name);
+    int begin_update(char *name);
+    int begin_delete(char *name);
+    int end_operation(void);
+    int begin_profile(void);
+    int end_profile(char *logfile); 
+}
